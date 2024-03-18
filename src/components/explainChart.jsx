4918deb0b468fa5893922bd1777e28aa2a9bae2c
babinAtInnovik <babin@innoviktechnologies.net>
2024-03-18 20:28:12 +0530

    add remarkstatus and some changes

--- src/components/explainChart.jsx
+++ src/components/explainChart.jsx
@@ -1,77 +1,109 @@
 import React, { Component } from 'react';
-import CanvasJSReact from '@canvasjs/react-charts';
-
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import ReactApexChart from 'react-apexcharts';
 
 class Fullchart extends Component {
-	constructor() {
-		super();
-		this.state = {
-			data: []
-		};
-		this.addSymbols = this.addSymbols.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        };
+        this.addSymbols = this.addSymbols.bind(this);
+    }
 
-	componentDidMount() {
-		this.fetchData(); 
-	}
+    componentDidMount() {
+        this.fetchData(); 
+    }
 
-	componentDidUpdate(prevProps) {
-		if (prevProps.moduleId !== this.props.moduleId) {
-			this.fetchData(); 
-		}
-	}
+    componentDidUpdate(prevProps) {
+        if (prevProps.moduleId !== this.props.moduleId) {
+            this.fetchData(); 
+        }
+    }
 
-	fetchData() {
-		const { moduleId } = this.props;
-		fetch(`http://68.178.175.207:8000/get_LMS_sdlc_phase_list?module_id=${moduleId}`)
-			.then(response => response.json())
-			.then(data => {
-				this.setState({ data });
-			})
-			.catch(error => {
-				console.log(error);
-			});
-	}
+    fetchData() {
+        const { moduleId } = this.props;
+        fetch(`http://68.178.175.207:8000/get_LMS_sdlc_phase_list?module_id=${moduleId}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ data });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
-	addSymbols(e) {
-		return e.value.toFixed(0) + "%"; 
-	}
+    addSymbols(e) {
+        return e.value.toFixed(0) + "%"; 
+    }
 
-	render() {
-		const { data } = this.state;
-        const { moduleName } = this.props;
-		const options = {
-			animationEnabled: true,
-			theme: "light2",
-			title: {
-				text: moduleName
-			},
-			axisY: {
-				title: "Percentage (%)", 
-				labelFormatter: this.addSymbols,
-				minimum: 0, 
-				maximum: 100 
-			},
-			axisX: {
-				title: "",
-				labelAngle: 0
-			},
-			data: [{
-				type: "column",
-				dataPoints: data.map(item => ({ label: item.sdlc_type, y: item.weighted_percent }))
-			}]
-		}
+    render() {
+        const { data } = this.state;
+        const { moduleName, colors } = this.props;
 
-		return (
-			<div className='mt-5'>
-				<div className='container ' style={{width: "100%"}}>
-					<CanvasJSChart options={options} />
-				</div>
-			</div>
-		);
-	}
+        const seriesData = {
+            data: data.map(item => item.weighted_percent)
+        };
+
+        const options = {
+            chart: {
+                height: 350,
+                type: 'bar',
+                events: {
+                    click: function(chart, w, e) {
+                        // console.log(chart, w, e)
+                    }
+                }
+            },
+            colors: colors,
+            plotOptions: {
+                bar: {
+                    columnWidth: '45%',
+                    distributed: true,
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            legend: {
+                show: false
+            },
+            xaxis: {
+                categories: data.map(item => item.sdlc_type),
+                labels: {
+                    style: {
+                        colors: colors,
+                        fontSize: '12px'
+                    }
+                }
+            },
+            yaxis: {
+                min: 0,
+                max: 100,
+                labels: {
+                    formatter: function(val) {
+                        return val.toFixed(0) + "%";
+                    }
+                }
+            },
+            title: {
+                text: moduleName,
+                floating: true,
+                offsetY: 330,
+                align: 'center',
+                style: {
+                    color: '#444'
+                }
+            }
+        };
+
+        return (
+            <div className='mt-5'>
+                <div className='container' style={{width: "100%"}}>
+                    <ReactApexChart options={options} series={[seriesData]} type="bar" height={350} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Fullchart;
--- src/components/progress-bar.jsx
+++ src/components/progress-bar.jsx
@@ -5,68 +5,79 @@ import Fullchart from "./explainChart";
 import { Accordion } from "react-bootstrap";
 
 const ProgressBar = () => {
-  const colors = ["#FF6B6B", "#48DBFB", "#FF9F40", "#FFD700", "#C4E538"];
   const [moduleData, setModuleData] = useState([]);
+  const [colors, setColors] = useState([]);
 
   const fetchInitialData = async () => {
     try {
       const response = await axios.get("http://68.178.175.207:8000/get_LMS_all_modules_list");
       const initialModuleData = response.data.map(module => ({ ...module, completion: 0 }));
       setModuleData(initialModuleData);
+      generateColors(initialModuleData.length);
     } catch (error) {
       console.error("Error fetching module data:", error);
     }
   };
 
-  const fetchProgress = async (moduleId) => {
-    try {
-      const response = await axios.get(`http://68.178.175.207:8000/get_LMS_sdlc_weighted_percent_sum?module_id=${moduleId}`);
-      const progressData = response.data;
-      setModuleData(prevModuleData => {
-        return prevModuleData.map(module => {
-          if (module.module_id === moduleId) {
-            return { ...module, completion: progressData[0].overall_percentage };
-          }
-          return module;
-        });
-      });
-    } catch (error) {
-      console.error("Error fetching progress data:", error);
+  const generateRandomColor = () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
+  const generateColors = (numberOfColors) => {
+    const generatedColors = [];
+    for (let i = 0; i < numberOfColors; i++) {
+      generatedColors.push(generateRandomColor());
     }
+    setColors(generatedColors);
   };
 
   useEffect(() => {
     fetchInitialData();
   }, []);
 
-  const handleAccordionChange = async (moduleId) => {
-    await fetchProgress(moduleId);
+  const getStatusColor = (status) => {
+    const index = moduleData.findIndex(module => module.remarks === status);
+    if (index !== -1) {
+      return colors[index % colors.length];
+    } else {
+      return "black"; // Default color
+    }
   };
+ 
+
 
   return (
     <div className="bars-main container">
       <div className="container">
         <div className="bars-container">
           <h1 className="pb-5">Modules <span className="Progress-heading">Progress Percentage  Tracker</span></h1>
-          <Accordion defaultActiveKey="0" >
+          <Accordion defaultActiveKey="0">
             {moduleData.map((module, index) => (
-               console.log(module),
-              <Accordion.Item key={index} eventKey={module.module_id} onClick={() => handleAccordionChange(module.module_id)}>
-              <span>{module.module_name}</span> 
-                <Accordion.Header >
+              <Accordion.Item key={index} eventKey={module.module_id}>
+                <span>{module.module_name}</span>
+                <Accordion.Header>
                   <span className="percentage">{`${module.overall_percentage}%`}</span>
                   <div className="progress-bar">
-                <div className="bar-container">
-                  <div className="bar" style={{ width: `${module.overall_percentage}%`, backgroundColor: colors[index] }}> 
-                  <span className="percentage2">{`${module.overall_percentage}%`}</span>
+                    <div className="bar-container">
+                      <div className="bar" style={{ width: `${module.overall_percentage}%`, backgroundColor: colors[index] }}>
+                        <span className="percentage2">{`${module.overall_percentage}%`}</span>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              
-              </div>
                 </Accordion.Header>
                 <Accordion.Body>
-                
-                  <Fullchart moduleId={module.module_id} moduleName={module.module_name} />
+                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <div style={{ width: '70%' }}>
+                      <div className="fullchart-container">
+                      <Fullchart moduleId={module.module_id} moduleName={module.module_name} colors={colors} />
+
+                      </div>
+                    </div>
+                    <div style={{ width: '30%', display: 'flex', alignItems: 'center' }}>
+                     <strong style={{ marginRight: '15px'}}>Remarks Status:</strong> <span style={{ marginRight: '5px', backgroundColor: getStatusColor(module.remarks), width: '10px', height: '10px', borderRadius: '50%', display: 'inline-block' }}></span>
+                       {module.remarks ? module.remarks : "No remarks"}
+                    </div>
+                  </div>
                 </Accordion.Body>
               </Accordion.Item>
             ))}
